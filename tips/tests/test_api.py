ea81643a2876b163a2944529e7a28181b1d14494
Johan Otten <drakonen@drakonen.com>
2020-12-24 10:26:34 +0100

    fixed tests

--- tips/tests/test_api.py
+++ tips/tests/test_api.py
@@ -40,7 +40,7 @@ class ApiTests(TestCase):
 
         tips = response.get_json()
 
-        self.assertEqual(5, len(tips))
+        self.assertEqual(6, len(tips))
 
         self.assertEqual(tips[0]['title'], 'Laat geen geld liggen')
         self.assertEqual(tips[0]['reason'], ['U ziet deze tip omdat u een TOZO aanvraag heeft gedaan'])
@@ -49,12 +49,13 @@ class ApiTests(TestCase):
         self.assertEqual(tips[1]['reason'], ['U ziet deze tip omdat u TOZO, stadspas of bijstandsuitkering heeft'])
 
         self.assertEqual(tips[2]['title'], 'Draag uw mondkapje')
+        self.assertEqual(tips[3]['title'], 'Hulp bij geldproblemen')
 
-        self.assertEqual(tips[3]['title'], 'Bekijk de afvalpunten in de buurt')
-        self.assertEqual(tips[3]['reason'], ['U ziet deze tip omdat u net bent verhuisd'])
+        self.assertEqual(tips[4]['title'], 'Bekijk de afvalpunten in de buurt')
+        self.assertEqual(tips[4]['reason'], ['U ziet deze tip omdat u net bent verhuisd'])
 
-        self.assertEqual(tips[4]['title'], 'Op stap met uw Stadspas')
-        self.assertEqual(tips[4]['reason'], ['U ziet deze tip omdat u een Stadspas hebt'])
+        self.assertEqual(tips[5]['title'], 'Op stap met uw Stadspas')
+        self.assertEqual(tips[5]['reason'], ['U ziet deze tip omdat u een Stadspas hebt'])
 
     def test_tips_audience(self):
         response = self.client.post('/tips/gettips?audience=zakelijk', json=get_fixture(optin=False))
