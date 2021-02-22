49866d06cfeb4c8297cad31677603b1304f826ab
Johan Otten <drakonen@drakonen.com>
2021-02-22 15:44:55 +0100

    New tips

--- tips/tests/test_api.py
+++ tips/tests/test_api.py
@@ -66,11 +66,11 @@ class ApiTests(TestCase):
 
         response = self.client.post('/tips/gettips?audience=persoonlijk', json=get_fixture_without_source_tips(optin=False))
         tips = response.get_json()
-        self.assertEqual(len(tips), 6)
+        self.assertEqual(len(tips), 7)
 
         response = self.client.post('/tips/gettips?audience=zakelijk,persoonlijk', json=get_fixture_without_source_tips(optin=False))
         tips = response.get_json()
-        self.assertEqual(len(tips), 11)
+        self.assertEqual(len(tips), 12)
 
     def test_income_tips(self):
         response = self.client.post('/tips/getincometips', json=self._get_client_data())
