24541d9a0956d3a75e1434b76ef9ff6e8840a43f
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2022-02-23 10:36:07 +0100

    Mijn-4357 Nieuwe tips voor verkiezingen 16-03-22 (#53)
    * fix: add new tips
    
    * fix: date is included
    
    * fix: update test
    
    * test: update
    
    * fix: update order

--- tests/test_api.py
+++ tests/test_api.py
@@ -86,14 +86,14 @@ class ApiTests(TestCase):
             json=get_fixture_without_source_tips(optin=False),
         )
         tips = response.get_json()
-        self.assertEqual(len(tips), 5)
+        self.assertEqual(len(tips), 8)
 
         response = self.client.post(
             "/tips/gettips?audience=zakelijk,persoonlijk",
             json=get_fixture_without_source_tips(optin=False),
         )
         tips = response.get_json()
-        self.assertEqual(len(tips), 10)
+        self.assertEqual(len(tips), 13)
 
     @freeze_time("2021-06-15")
     def test_income_tips(self):
